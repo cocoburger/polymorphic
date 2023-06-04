a2c2045412f935b4da36a61146f9e996909d2489
burger <fsm15@naver.com>
2023-06-04 17:48:26 +0900

    docker-compose dev, prod, backup용 파일 생성 및 설정 docker-compose arg 추가

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,6 +3,7 @@
  * 2. as prop의 기본값은 span으로 지정한다.
  * 3. as prop의 값에 따라 다른 태그를 렌더링 할 수 있어야 한다.
  */
+import {Button} from "./Button";
 
 type TextProps<T extends React.ElementType> = {
   as?: T;
@@ -25,8 +26,9 @@ export default function Index() {
         <Text as='h1'>Span Text</Text>
         <Text as='h2'>nginx로 배포 완료했나요?</Text>
         <Text as='h3'>
-          {`hello ${process.env.KOREAN_FOOD} 새롭게 추가한 문구입니다.`}
+          {`hello ${process.env.REACT_APP_NAME} 새롭게 추가한 문구입니다.`}
         </Text>
+        <Button>ㅎㅇ</Button>
       </>
   );
 }
